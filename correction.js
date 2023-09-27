83c36854515394075fe4640e239533d31055028e
Eskan0r1 <144712553+Eskan0r1@users.noreply.github.com>
2023-09-27 17:54:44 +0200

    Add files via upload

---
+++ correction.js
@@ -0,0 +1,18 @@
+  <script type="text/javascript">
+
+
+var q1= 0;
+var q2= 0;
+var q3= 0;
+var q4= 0;
+var q5= 0;
+var q6= 0;
+var q7= 0;
+var q8= 0;
+var q9= 0;
+var q10=0;
+//calcul du score
+function calcul() {score=q1+q2+q3+q4+q5+q6+q7+q8+q9+q10;}
+
+//commentaire du résultat
+function resultat() {calcul();
